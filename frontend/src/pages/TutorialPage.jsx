7b6633ffb6607f30f62cc91b692d10785cf90991
Benjamin Lee <benjamin.j.lee04@gmail.com>
2026-02-10 20:24:56 -0600

    Began writing the tutorial, not done though.

--- frontend/src/pages/TutorialPage.jsx
+++ frontend/src/pages/TutorialPage.jsx
@@ -16,13 +16,13 @@ function TutorialPage() {
 
     return ( // Build page and return it
         <div className="min-h-screen bg-base-300 p-6">
-            <h1 className="text-2xl font-bold mb-4">Start Page</h1>
+            <h1 className="text-2xl font-bold mb-4">Tutorial</h1>
             
             {/* Controls */}
             <div className="flex gap-2 mb-6">
-                <h1 className="text-xl font-semibold">Welcome to the Chord Builder! Press the button below to get started.</h1>
+                <h1 className="text-xl font-semibold">Step 1 Press the button below to get started.</h1>
             </div>
-            <button tutorial="btn btn-primary" onClick={changePage} disabled={false}>Go To Chord Builder</button>
+            <button tutorial="btn btn-primary" onClick={changePage} disabled={false}>Tutorial</button>
         </div>
         
     );
